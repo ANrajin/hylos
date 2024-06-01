a889c0020286675f5680af8ba029e5a59a2d8996
anrajin <an.rajin@gmail.com>
2024-06-01 12:47:53 +0600

    [update]-header and sidebar dark mode styles added

--- src/app/admin/layouts/header/header.component.ts
+++ src/app/admin/layouts/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Renderer2 } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
 import { Images } from 'src/assets/data/images';
 
 @Component({
@@ -6,9 +6,20 @@ import { Images } from 'src/assets/data/images';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  ngOnInit(): void {
+    const html = document.querySelector('html')!;
+    const isLightOrAuto = localStorage.getItem('hs_theme') === 'light' || (localStorage.getItem('hs_theme') === 'auto' && !window.matchMedia('(prefers-color-scheme: dark)').matches);
+    const isDarkOrAuto = localStorage.getItem('hs_theme') === 'dark' || (localStorage.getItem('hs_theme') === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches);
+
+    if (isLightOrAuto && html.classList.contains('dark')) html.classList.remove('dark');
+    else if (isDarkOrAuto && html.classList.contains('light')) html.classList.remove('light');
+    else if (isDarkOrAuto && !html.classList.contains('dark')) html.classList.add('dark');
+    else if (isLightOrAuto && !html.classList.contains('light')) html.classList.add('light');
+  }
+
   public userOne: string = Images.users.userOne;
-  
+
   isOpen: boolean = false;
 
   constructor(private element: ElementRef, private renderer: Renderer2) {}
